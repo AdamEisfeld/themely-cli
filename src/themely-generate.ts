819fc0fde8ae5b81fd66d48d66145663be90b03e
Adam Eisfeld <adam.eisfeld@gmail.com>
2021-06-10 23:14:50 -0400

    - removing direct .command creation on themely-generate

--- src/themely-generate.ts
+++ src/themely-generate.ts
@@ -9,8 +9,6 @@ import * as SignalUtils from './utils/SignalUtils';
 
 export const setupThemelyGenerate = function(program: any) {
     program
-    .command('css', 'build a css file', { executableFile: 'themely-generate-css' })
-    .command('stylus', 'build a stylus file', { executableFile: 'themely-generate-stylus' })
     .configureOutput({
         outputError: (str: string) => SignalUtils.signalError('SYNTAX ERROR', new Error(`${SignalUtils.cleanup(str)}`))
     });
